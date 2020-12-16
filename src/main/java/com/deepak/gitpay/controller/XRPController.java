9deae16ef6563b2eda78ad6385a80d102bb9b4b5
Deepak <deepak.valluru@aa.com>
2020-12-15 22:10:35 -0600

    Removed test yml file.
    Pay id is deepakvalluru$ripplex.money

--- src/main/java/com/deepak/gitpay/controller/XRPController.java
+++ src/main/java/com/deepak/gitpay/controller/XRPController.java
@@ -26,7 +26,6 @@ import java.io.StringReader;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
