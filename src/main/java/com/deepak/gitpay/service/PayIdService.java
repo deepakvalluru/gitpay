2f8856d679d6d9657790b5861cd95c8a4cc3436e
Deepak <deepak.valluru@aa.com>
2021-01-02 00:03:19 -0600

    Updated Action yml file and workflow files pay$wietse.com

--- src/main/java/com/deepak/gitpay/service/PayIdService.java
+++ src/main/java/com/deepak/gitpay/service/PayIdService.java
@@ -97,6 +97,7 @@ public class PayIdService
       System.out.println("XRP Amount distributed per commit = " + xrpAmountPerCommit);
       System.out.println("ETH Amount distributed per commit = " + ethAmountPerCommit);
 
+
       for( String commitId : commitPayIdMap.keySet() )
       {
          List<String> payIdsForThisCommit = commitPayIdMap.get(commitId);
